fb9728cf34362a8b80bf4d4e54d94210eace5f5f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-02 12:11:39 +0100

    Create HeaderGraph.jsx

---
+++ src/Components/Main/HeaderGraph.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const HeaderGraph = () => {
+
+    return (
+        <section className="">
+            <div className="">
+                <div className="p-4 bg-white shadow-lg rounded-lg">
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeaderGraph;
