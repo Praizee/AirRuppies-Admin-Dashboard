8784d4fcc22aaa54c8d52c114e655dd01f118086
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-09 11:50:46 +0100

    Added Roles and Permissions TableData
    Added TableData for Roles and Permissions pages

--- src/Components/Main/content.jsx
+++ src/Components/Main/content.jsx
@@ -395,3 +395,69 @@ export const AccountsTableData = [
         date: "13 July 2021",
     },
 ]
+
+export const RolesTableData = [
+    {
+        user: "Williams Temitope",
+        email: "susanna.Lind57@gmail.com",
+        role: "Admin",
+        status: "Inactive",
+    },
+    {
+        user: "Williams Temitope",
+        email: "estelle.Bailey10@gmail.com",
+        role: "Admin",
+        status: "Active",
+    },
+    {
+        user: "Williams Temitope",
+        email: "milo86@hotmail.com",
+        role: "Admin",
+        status: "Active",
+    },
+    {
+        user: "Williams Temitope",
+        email: "lonnie35@hotmail.com",
+        role: "Maintainer",
+        status: "Inactive",
+    },
+    {
+        user: "Williams Temitope",
+        email: "ahmad_Collins@yahoo.com",
+        role: "Maintainer",
+        status: "Active",
+    },
+]
+
+export const PermissionsTableData = [
+    {
+        name: "Williams Temitope",
+        assignedTo: "Manager",
+        createdDate: "14 Apr 2021, 8:43 PM",
+    },
+    {
+        name: "Williams Temitope",
+        assignedTo: "User",
+        createdDate: "14 Apr 2021, 8:43 PM",
+    },
+    {
+        name: "Williams Temitope",
+        assignedTo: "Support",
+        createdDate: "14 Apr 2021, 8:43 PM",
+    },
+    {
+        name: "Williams Temitope",
+        assignedTo: "Manager",
+        createdDate: "14 Apr 2021, 8:43 PM",
+    },
+    {
+        name: "Williams Temitope",
+        assignedTo: "Restricted User",
+        createdDate: "14 Apr 2021, 8:43 PM",
+    },
+    {
+        name: "Williams Temitope",
+        assignedTo: "Manager",
+        createdDate: "14 Apr 2021, 8:43 PM",
+    },
+]
