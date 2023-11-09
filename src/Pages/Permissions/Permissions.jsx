7b23e36cebb6874e5229024d48734ec013c926ed
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-09 11:49:09 +0100

    Updated Permissions page content
    Updated Permissions page content with modal and table

--- src/Pages/Permissions/Permissions.jsx
+++ src/Pages/Permissions/Permissions.jsx
@@ -2,20 +2,53 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAppContext } from '../../Context/AppContext';
 import { getDatabase, ref, set } from 'firebase/database';
+import { BiExport } from "react-icons/bi";
+import { GoSearch } from "react-icons/go";
+import { PermissionsTable } from './PermissionsTable';
+import { AddPermissionsModal } from './AddPermissionsModal';
 
 
 const Permissions = () => {
     return (
-        <div className="container pt-20 pb-0">
+        <section>
+            <div className="container pt-12 pb-0">
+                <span className=''>
+                    <h1 className="text-[1.5rem] mt-4 text-[#2E2E3A] leading-normal font-bold">
+                        Permission List
+                    </h1>
+                    <p className='text-[#32475C99] text-sm font-semibold'>
+                        A permission provided access to predefined menus
+                        and features so that depending on assigned permission
+                        to an administrator can have access to what he needs
+                    </p>
+                </span>
 
-            <div className='bg-white px-4 py-8 tablet:p-6 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
-                <h1 className="text-[2rem] text-center mt-4 mb-2 text-[#2E2E3A] leading-normal font-bold">
-                    Permissions
-                </h1>
-
-
+                <div className='bg-white space-y-4 rounded-lg overflow-x-auto mt-8 p-4 shadow-md'>
+                    <div className='flex justify-between'>
+                        <div></div>
+                        <div className='flex gap-4'>
+                            <div className="relative">
+                                <input
+                                    type="text"
+                                    id="Search"
+                                    placeholder="Search Permissions"
+                                    className="w-full rounded-md border-[#D6DDEB] py-3.5 pe-10 shadow-sm sm:text-sm"
+                                />
+                                <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+                                    <button type="button">
+                                        <GoSearch />
+                                    </button>
+                                </span>
+                            </div>
+                            <div>
+                                <AddPermissionsModal />
+                            </div>
+                        </div>
+                    </div>
+                    <PermissionsTable />
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
